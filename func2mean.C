9748d279d37d0cf766bc6887cdbfe9ab471abec7
Daniel Magdalinski <da1162ma-s@student.lu.se>
2019-11-10 15:47:30 +0100

    Added the attempt at fitting the graph and som comments.

--- func2mean.C
+++ func2mean.C
@@ -25,7 +25,7 @@ void func2mean(){
 	Int_t beginYear=1722;//Starting year
 	Int_t endYear=2013;//Ending year
 	Int_t bins=2013-1722;
-	Int_t nAvgPoints=10;// Number of average points used to graph the moving average.
+	Int_t nAvgPoints=20;// Number of average points used to graph the moving average.
 	TH1D* hist = new TH1D("hist", "Histogram", bins, beginYear, endYear);// Histogram for the data
 	TH1D* hist2 = new TH1D("hist", "Histogram", bins, beginYear, endYear);//Histogram to create the blue below average effect.
 	TH1D* hist3 = new TH1D("hist", "Histogram", bins, beginYear, endYear);//Histogram to add white over the blue.
@@ -79,14 +79,14 @@ void func2mean(){
 	}
 			
 	
-	//TCanvas *cs = new TCanvas();
+	TCanvas *cs = new TCanvas();
 	// To plot this deviation from average effect we use THStack
 	THStack *hs = new THStack("hs","");
 	//First histogram is the red one with all data.
 	hist->SetFillColor(kRed);
 	hist->SetLineColor(kBlack);
 	hist->SetLineWidth(1);
-	hist->SetMinimum(3);
+	hist->SetMinimum(2);
 	hist->SetMaximum(8);
 	hs->Add(hist);
 	
@@ -104,8 +104,7 @@ void func2mean(){
 	
 	hs->Draw("nostack");//Nostack option means that their values don't stack.
 	
-	TGraph *gr=new TGraph(bins-nAvgPoints,x,y);
-	gr->SetLineWidth(4);
+	
 	
 	hs->SetTitle("Yearly Mean Temperature, Uppsala 1722-2013");
     hs->GetXaxis()->SetTitle("Year");
@@ -113,6 +112,101 @@ void func2mean(){
     hs->GetXaxis()->CenterTitle(kTRUE);
     hs->GetYaxis()->CenterTitle(kTRUE);
     hs->SetMinimum(2);
+    
+    
+    TGraph *gr=new TGraph(bins-nAvgPoints,x,y);
+	gr->SetLineWidth(4);
 	gr->Draw("SAME C");
 	
+	
+	/*
+	// Fit with a smaller amount of gaussians, follows the moving average with a smoothe function but does not give a good future prediction.
+	Double_t par[21];
+	TF1 *g1 = new TF1("g1","gaus",1722+nAvgPoints/2,1755);
+	TF1 *g2 = new TF1("g2","gaus",1755,1785);
+	TF1 *g3 = new TF1("g3","gaus",1785,1837);
+	TF1 *g4 = new TF1("g4","gaus",1839,1870);
+	TF1 *g5 = new TF1("g5","gaus",1880,1925);
+	TF1 *g6 = new TF1("g6","gaus",1927,1958);
+	TF1 *g7 = new TF1("g7","gaus",1960,2013-nAvgPoints/2);
+	TF1 *total = new TF1("total","gaus(0)+gaus(3)+gaus(6)+gaus(9)+gaus(12)+gaus(15)+gaus(18)",1722,2013);
+	total->SetLineColor(1);
+	total->SetLineWidth(4);
+	gr->Fit(g1,"R0");
+	gr->Fit(g2,"R+0");
+    gr->Fit(g3,"R+0");
+	gr->Fit(g4,"R+0");
+	gr->Fit(g5,"R+0");
+	gr->Fit(g6,"R+0");
+	gr->Fit(g7,"R+0");
+	g1->GetParameters(&par[0]);
+	g2->GetParameters(&par[3]);
+	g3->GetParameters(&par[6]);
+	g4->GetParameters(&par[9]);
+	g5->GetParameters(&par[12]);
+	g6->GetParameters(&par[15]);
+	g7->GetParameters(&par[18]);
+
+	total->SetParameters(par);
+	gr->Fit(total,"R+");
+	*/
+	
+	
+	
+	
+	
+	
+	/*
+	
+	//Fitting the graph with a lot of gaussian, a bit to many for the fit to be appropriate.
+	Double_t par[42];
+	TF1 *g1 = new TF1("g1","gaus",1730,1755);
+	TF1 *g2 = new TF1("g2","gaus",1757,1768);
+	TF1 *g3 = new TF1("g3","gaus",1771,1782);
+	TF1 *g4 = new TF1("g4","gaus",1785,1805);
+	TF1 *g5 = new TF1("g5","gaus",1810,1840);
+	TF1 *g6 = new TF1("g6","gaus",1840,1868);
+	TF1 *g7 = new TF1("g7","gaus",1868,1879);
+	TF1 *g8 = new TF1("g8","gaus",1879,1900);
+	TF1 *g9 = new TF1("g9","gaus",1900,1926);
+	TF1 *g10 = new TF1("g10","gaus",1930,1939);
+	TF1 *g11 = new TF1("g11","gaus",1943,1955);
+	TF1 *g12 = new TF1("g12","gaus",1965,1980);
+	TF1 *g13 = new TF1("g13","gaus",1981,2013-nAvgPoints/2);
+	TF1 *g14 = new TF1("g14","gaus",1722+nAvgPoints/2,1730);
+	TF1 *total = new TF1("total","gaus(0)+gaus(3)+gaus(6)+gaus(9)+gaus(12)+gaus(15)+gaus(18)+gaus(21)+gaus(24)+gaus(27)+gaus(30)+gaus(33)+gaus(36)+gaus(39)",1722,2013);
+	total->SetLineColor(1);
+	total->SetLineWidth(4);
+	gr->Fit(g1,"R0");
+	gr->Fit(g2,"R+0");
+    gr->Fit(g3,"R+0");
+	gr->Fit(g4,"R+0");
+	gr->Fit(g5,"R+0");
+	gr->Fit(g6,"R+0");
+	gr->Fit(g7,"R+0");
+	gr->Fit(g8,"R+0");
+	gr->Fit(g9,"R+0");
+	gr->Fit(g10,"R+0");
+	gr->Fit(g11,"R+0");
+	gr->Fit(g12,"R+0");
+	gr->Fit(g13,"R+0");
+	gr->Fit(g14,"R+0");
+	g1->GetParameters(&par[0]);
+	g2->GetParameters(&par[3]);
+	g3->GetParameters(&par[6]);
+	g4->GetParameters(&par[9]);
+	g5->GetParameters(&par[12]);
+	g6->GetParameters(&par[15]);
+	g7->GetParameters(&par[18]);
+	g8->GetParameters(&par[21]);
+	g9->GetParameters(&par[24]);
+	g10->GetParameters(&par[27]);
+	g11->GetParameters(&par[30]);
+	g12->GetParameters(&par[33]);
+	g13->GetParameters(&par[36]);
+	g14->GetParameters(&par[39]);
+
+	total->SetParameters(par);
+	gr->Fit(total,"R+");
+	*/
 }
